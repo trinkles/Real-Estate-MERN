062978e3e802176e350dbb00e171dc673147dae4
trinkles <cmc0234@dlsud.edu.ph>
2024-09-16 23:21:18 +0800

    Created route for test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose
@@ -19,6 +20,4 @@ app.listen(3000, () => {
 });
 
 // test api... could be improved
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
-});
+app.use('/api/user', userRouter);
