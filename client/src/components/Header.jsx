dba2edf0d962d1b332c9af63c01d2b2e8d3a2991
trinkles <cmc0234@dlsud.edu.ph>
2024-09-16 19:17:06 +0800

    Installed React Icons; created nav bar

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="bg-rose-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-rose-700">Her</span>
+            <span className="text-rose-400">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-rose-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-rose-400" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-rose-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-rose-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="text-rose-700 hover:underline">Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
